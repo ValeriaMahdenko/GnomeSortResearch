c6fbf5be05132653a652d1464d36ee541347a150
Sophia Shuvar <45092554+sophiashuv@users.noreply.github.com>
2021-03-08 18:56:10 +0200

    Update GnomeSort.h

--- sorting_algorithms/GnomeSort.h
+++ sorting_algorithms/GnomeSort.h
@@ -1,17 +1,31 @@
+#pragma once
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void GnomeSort(int* data, int count) {
-	for (int i = 1; i < count; ) {
-		if (data[i - 1] <= data[i])
-			++i;
-		else {
-			int tmp = data[i];
-			data[i] = data[i - 1];
-			data[i - 1] = tmp;
-			--i;
-			if (i == 0)
-				i = 1;
-		}
-	}
+auto GnomeSort(int* data, int count) {
+//    for (int i = 1; i < count; ) {
+//        if (data[i - 1] <= data[i])
+//            ++i;
+//        else {
+//            int tmp = data[i];
+//            data[i] = data[i - 1];
+//            data[i - 1] = tmp;
+//            --i;
+//            if (i == 0)
+//                i = 1;
+//        }
+//    }
+
+    int index = 0;
+
+    while (index < count) {
+        if (data[index] >= data[index - 1] || index == 0)
+            index++;
+        else {
+            swap(data[index], data[index - 1]);
+            index--;
+        }
+    }
+
 }
